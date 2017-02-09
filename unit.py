65c907b6821945f4b18532c069b53f777e2f6f7c
Kaylee Moser <kaylee@mevoked.com>
2017-02-08 22:14:46 -0800

    unittest for update functions in Contact class

--- unit.py
+++ unit.py
@@ -17,10 +17,22 @@ class TestContactClass(unittest.TestCase):
     def setUp(self):
         self.user = birthday.Contact("Harry","07-31-1980","4 Privet Drive, Little Whinging, Surrey")
 
-    def testContactCreate(self):
+    def testCreateContact(self):
         self.assertEqual(self.user.name,"Harry")
         self.assertEqual(self.user.birthday,"07-31-1980")
         self.assertEqual(self.user.address,"4 Privet Drive, Little Whinging, Surrey")
+
+    def testUpdateContact(self):
+        self.user.setName("Ron")
+        self.user.setBirthday("03-01-1980")
+        self.user.setAddress("The Burrow, Ottery St. Catchpole, Devon")
+        self.assertEqual(self.user.name,"Ron")
+        self.assertEqual(self.user.birthday,"03-01-1980")
+        self.assertEqual(self.user.address,"The Burrow, Ottery St. Catchpole, Devon")
+
+    def tearDown(self):
+        #need to make sure it goes back to the way it was when first set
+        self.user = birthday.Contact("Harry","07-31-1980","4 Privet Drive, Little Whinging, Surrey")
 
 
 if __name__ == '__main__':
